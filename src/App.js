6fe891c12c42461f95ed10a78bbbe9ae316e550a
Ravindra Kawale <ravindra.kawale@waiin.com>
2024-05-21 16:47:08 +0530

    video 10 completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About TextUtils" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" />
+        {/* <TextForm heading="Enter the text to analyze" /> */}
+        <About />
       </div>
     </>
   );
